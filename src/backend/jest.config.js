cd9e54d5f736c770c43039dca39485c2a0c2d70e
Adzfar <md.nur.adzfar@gmail.com>
2025-08-28 22:39:57 +0800

    Cleaned up backend folder

--- src/backend/jest.config.js
+++ src/backend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest', // Use ts-jest preset
   testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.ts'], // Adjust if your tests are .js
+  testMatch: ['**/__tests__/**/*.test.ts'], // Adjust if your tests are .js
   // You might not need 'transform' if 'preset' is used, but it's harmless if present.
   transform: {
     '^.+\\.ts$': 'ts-jest',
